6a681e04e4bf964054395b3da7620df4a135e4cf
Mauro Rigo <mrigo000@login05.leonardo.local>
2024-09-06 18:11:58 +0200

    Testing jax and jaxlib versions

--- setup.py
+++ setup.py
@@ -112,8 +112,8 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
-        "jax>=0.4.16,<=0.4.20",
-        "jaxlib>=0.4.16,<=0.4.20",
+        "jax>=0.4.16,<=0.4.23",
+        "jaxlib>=0.4.16,<=0.4.23",
         "numpy<2",
         "mpi4jax>=0.4.0"
     ],
